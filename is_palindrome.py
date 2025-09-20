50a20ccef1a05b0cadad6a18fc6946b97d9b6f93
VirkoDeerman <robertviva2019mail.com>
2025-09-20 22:15:27 +0200

    fixed exception message

--- is_palindrome.py
+++ is_palindrome.py
@@ -2,6 +2,6 @@ def is_palindrome() -> bool:
 	if usput := ''.join(char for char in input('Enter something:\n> ').lower() if char.isalpha()):
 		return usput == usput[::-1]
 	else:
-		raise SystemExit('No imput próvided, ending the program.')
+		raise SystemExit('No imput provided, ending the program.')
 
 print('Yep, it\'s a palindrome, the thing you wrote.' if is_palindrome() else 'Nah, that wasn\'t a palindrome, nuh huh.')
