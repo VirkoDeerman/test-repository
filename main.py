d6e51f18a17cfe56a74c0d617c86ccb2e094ac68
VirkoDeerman <robertviva2019mail.com>
2025-09-20 19:40:30 +0200

    final blow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from random import choice
-msg: list[str] = [
+print(choice([
 	'Hi!\nWhat can I do for you?',
 	'Hello.\nHow may I help?',
 	'...\nTell me what to do.'
-]
-print(choice(msg))
-
-#pipiska negra trololo tralala
+]))
