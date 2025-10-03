23770c5a47cad301244c44515b4f9e853e75a1ff
VirkoDeerman <robertviva2019@gmail.com>
2025-10-03 16:24:47 +0200

    I'm very sorry

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,7 +1,8 @@
 def is_palindrome() -> bool:
-	if usput := ''.join(char for char in input('Enter something:\n> ').lower() if char.isalpha()):
+	if usput := ''.join(char for char in input('\nEnter something:\n> ').lower() if char.isalpha()):
 		return usput == usput[::-1]
 	else:
 		raise SystemExit('No imput provided, ending the program.')
-while input('Yep, it\'s a palindrome, the thing you wrote.' if is_palindrome() else 'Nah, that wasn\'t a palindrome, nuh huh.'):
+while True:
+	print('Yep, it\'s a palindrome, the thing you wrote.' if is_palindrome() else 'Nah, that wasn\'t a palindrome, nuh huh.'):
 	pass
